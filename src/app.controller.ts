e97f4821a9ab9e7f4c966580a803b8068d4484e4
lshyun955 <lshyun955@gmail.com>
2022-08-23 18:34:07 +0900

    Add: 고양이 커뮤니티 Mongodb Schema 설정 완료

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Request } from '@nestjs/common';
+import { CatsService } from './cats/cats.service';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly catsService: CatsService,
+  ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.catsService.hiCatServiceProduct();
   }
 }
---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,49 @@
+import { CatsService } from './cats.service';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { PositveIntPipe } from 'src/common/pipes/positiveInt.pipe';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+
+@Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getCurrentCat() {
+    return 'current cat';
+  }
+
+  @Post()
+  async signUp() {
+    return 'signup';
+  }
+
+  @Post('login')
+  login() {
+    return 'login';
+  }
+
+  @Post('logout')
+  logOut() {
+    return 'logout';
+  }
+
+  @Post('upload/cats')
+  uploadCatImg() {
+    return 'uploadImg';
+  }
+}
