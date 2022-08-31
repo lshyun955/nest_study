a357c5188e44ae14ec751cac2c5654f78642bbea
lshyun955 <lshyun955@gmail.com>
2022-09-01 08:28:46 +0900

    Add: Repository 디자인 패턴 적용

---
+++ src/cats/cats.repository.ts
@@ -0,0 +1,23 @@
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable, HttpException } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Cat } from './cats.schema';
+import { CatRequestDto } from './dto/cats.request.dto';
+
+@Injectable()
+export class CatsRepository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async existsByEmail(email: string): Promise<boolean> {
+    try {
+      const result = await this.catModel.exists({ email });
+      return result ? true : false;
+    } catch (error) {
+      throw new HttpException('db error', 400);
+    }
+  }
+
+  async create(cat: CatRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat);
+  }
+}
